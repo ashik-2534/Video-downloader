41dd33a4d596cc0bf1d1bedb7924950f7f175eb3
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-02-08 13:48:01 +0600

    the commit message for your changes. Lines starting

--- youtube_downloader/youtube_downloader/celery.py
+++
@@ -1,9 +0,0 @@
-# youtube_downloader/celery.py
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youtube_downloader.settings')
-
-app = Celery('youtube_downloader')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
